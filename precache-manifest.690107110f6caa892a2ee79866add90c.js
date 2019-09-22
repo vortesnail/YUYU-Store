abedcc7e33a5d2aa1f380bbe441b19360e524f77
vortesnail <1091331061@qq.com>
2019-09-22 22:47:27 +0800

    Main page have finished

---
+++ precache-manifest.690107110f6caa892a2ee79866add90c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c8c26e22e695829d5ccd0ae2212deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014a53f6c65569b787f2",
+    "url": "/static/js/2.f1ff6876.chunk.js"
+  },
+  {
+    "revision": "5d136bd7ac8490500b9f",
+    "url": "/static/js/main.4dced93e.chunk.js"
+  },
+  {
+    "revision": "1602fcc8f0662d400f7c",
+    "url": "/static/js/runtime-main.e4c298d4.js"
+  },
+  {
+    "revision": "e9420bca10988dc4aa2d3257f0b117db",
+    "url": "/static/media/logo.e9420bca.png"
+  }
+]);
